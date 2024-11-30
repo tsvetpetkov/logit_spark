60eb7ca5b3b450b707008e1ce16644e83e61f398
Tsvet P <126100226+tsvetpetkov@users.noreply.github.com>
2024-11-30 17:06:58 -0500

    Update reg_out.py

--- reg_out.py
+++ reg_out.py
@@ -1,16 +1,18 @@
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
+
 import pyspark.sql.functions as F
 
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import GeneralizedLinearRegression
 
-import math
 import pandas as pd
 import numpy as np
 
 from operator import add
 from functools import reduce
+
+import math
 
 
 # initialize data, outcome, regressors, and sample weights
@@ -26,14 +28,14 @@ w = 'wt'
 
 def mirror_diag_mat(vars_lbls, vals_dict):
     mat = []
-    for i_ind, i in enumerate(vars_lbls):
+    for i_idx, i in enumerate(vars_lbls):
         row = []
-        for j_ind, j in enumerate(vars_lbls):
-            if i_ind >= j_ind:
+        for j_idx, j in enumerate(vars_lbls):
+            if i_idx >= j_idx:
                 row.extend([vals_dict[i, j]])
             else:
                 row.extend([vals_dict[j, i]])
-        mat.extend([row])
+    mat.extend([row])
 
 
 # assemble data
@@ -53,7 +55,7 @@ data = data.withColumn(wt, F.col(wt) * obs_cnt / wt_sum)
 # collect logit beta coefs
 
 fit = GeneralizedLinearRegression(labelCol = y, featuresCol = 'X', weightCol = w, fitIntercept = True, \
-                                  family = 'binomial', link = 'logit', maxIter = 100, tol = 1e-8,).fit(data)
+                                  family = 'binomial', link = 'logit', maxIter = 100, tol = 1e-8).fit(data)
 
 expl_vars = ['cons'] + features
 beta_coefs = [fit.intercept] + fit.coefficients.tolist()
@@ -63,7 +65,8 @@ betas_dict = dict(zip(expl_vars, beta_coefs))
 # collect logit preds, resids, and classification
 
 data = data.withColumn('cons', F.lit(1))
-data = data.withColumn('Xb', reduce(add, [F.col(i) * betas_dict[i] for i in list(betas_dict.keys())]))
+data = data.withColumn('Xb', reduce(add, [F.col(i) * betas_dict[i] for i in expl_vars]))
+
 data = data.withColumn('pred', 1 / (1 + F.exp(-F.col('Xb'))))
 data = data.withColumn('resid', F.col(y) - F.col('pred'))
 data = data.withColumn('class', F.when(F.col('pred') > 0.5, 1).otherwise(0))
@@ -71,7 +74,7 @@ data = data.withColumn('class', F.when(F.col('pred') > 0.5, 1).otherwise(0))
 
 # collect logit beta coefs SEs
 
-expl_vars_pairs = [(i, j) for i_ind, i in enumerate(expl_vars) for j_ind, j in enumerate(expl_vars) if i_ind >= j_ind]
+expl_vars_pairs = [(i, j) for i_idx, i in enumerate(expl_vars) for j_idx, j in enumerate(expl_vars) if j_idx >= j_ind]
 
 sum_prods = data.select([F.sum(F.col(i[0]) * F.col(i[1]) * F.col(w) * F.col('pred') * (1 - F.col('pred'))) for i in expl_vars_pairs]).collect()[0]
 sum_prods_dict = dict(zip(expl_vars_pairs, sum_prods))
@@ -80,13 +83,28 @@ vcv_mat = np.linalg.inv(mirror_diag_mat(expl_vars, sum_prods_dict))
 std_errs_dict = dict(zip(expl_vars, np.sqrt(np.diag(vcv))))
 
 
+# collect marginal effects
+
+reg_vars = [y] + X
+sums = data.select([F.sum(F.col(i) * F.col(w)) for i in reg_vars]).collect()[0]
+
+means = [i / obs_cnt for i in sums]
+means_dict = dict(zip(reg_vars, means))
+
+marg_fx_dict = {i: betas_dict[i] * means_dict[y] * (1 - means_dict[y]) for i in expl_vars}
+
+
 # collect logit standardized beta coefs
+
+prod_sums = data.select([F.sum(F.pow(F.col(i) - means_dict[i], 2) * F.col(w)) for i in reg_vars]).collect()[0]
+
+std_devs = [math.sqrt(i / (obs_cnt - 1)) for i in prod_sums]
+std_devs_dict = dict(zip(reg_vars, std_devs))
 
 xb_avg = data.agg(F.sum(F.col('Xb') * F.col(w))).collect()[0][0] / obs_cnt
 xb_var = data.agg(F.sum(F.pow(F.col('Xb') - xb_avg, 2) * F.col(w))).collect()[0][0] / (obs_cnt - 1)
+
 ystar_var = xb_var + pow(math.pi, 2) / 3
 
-std_beta_coefs = [betas_dictp[i] * std_devs_dict[i] / math.sqrt(ystar_var) for i in X]
-std_betas_dict = dict(zip(X, std_beta_coefs))
-
+std_betas_dict = {i: betas_dict[i] * std_devs_dict[i] / math.sqrt(ystar_var) for i in X}
 
